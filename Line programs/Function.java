f48b094b4a4e407694179ccb3bf306ef012c93a0
pashtetys11061987 <72036347+pashtetys11061987@users.noreply.github.com>
2020-10-07 22:20:39 +0300

    Update Function.java

--- Line programs/Function.java
+++ Line programs/Function.java
@@ -1,1 +1,14 @@
+// Найдите значение функции: z=((a-3)*b/2)+c 
+
+class Function { 
+    public static void main(String[] args){
+        int z;
+        int a=5;
+        int b=10;
+        int c=15;
+
+        z=(a-3)*b/2+c;
+        System.out.println("z = "+z); 
+    } 
+} 
 
